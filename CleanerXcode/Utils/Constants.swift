65084750fa2ed13561a7a0a3c75660cbbc9de76a
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-03-31 17:44:38 -0300

    Make unit tests for constants class.

---
+++ CleanerXcode/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CleanerXcode
+//
+//  Created by Adriano Costa on 31/03/25.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let githubURL = URL(string: "https://github.com/didisouzacosta/CleanerXcode")!
+    static let xURL = URL(string: "https://x.com/didisouzacosta")!
+    static let linkedinURL = URL(string: "https://www.linkedin.com/in/adrianosouzacosta")!
+    
+}
