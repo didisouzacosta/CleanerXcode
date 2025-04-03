eaaa5a38571aec33108901e871a9e29d084a9a82
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-04-03 14:02:43 -0300

    Improving preferences tests checking default values

--- CleanerXcode/Utils/Preferences.swift
+++ CleanerXcode/Utils/Preferences.swift
@@ -10,52 +10,83 @@ import SwiftUI
 @Observable
 final class Preferences {
     
+    // MARK: - Public Variables
+    
     static let shared = Preferences()
     
-    var removeArchives = StoragedValue(
-        Command.removeArchives.script,
-        defaultValue: true
-    )
+    var removeArchives: StoragedValue<Bool>
+    var removeCaches: StoragedValue<Bool>
+    var removeDerivedData: StoragedValue<Bool>
+    var clearDeviceSupport: StoragedValue<Bool>
+    var removeOldSimulators: StoragedValue<Bool>
+    var clearSimulatorData: StoragedValue<Bool>
+    var resetXcodePreferences: StoragedValue<Bool>
+    var displayFreeUpSpaceInMenuBar: StoragedValue<Bool>
+    var launchAtLogin: StoragedValue<Bool>
     
-    var removeCaches = StoragedValue(
-        Command.removeCaches.script,
-        defaultValue: true
-    )
+    // MARK: - Private Variables
     
-    var removeDerivedData = StoragedValue(
-        Command.removeDerivedData.script,
-        defaultValue: true
-    )
+    private let userDefaults: UserDefaults
     
-    var clearDeviceSupport = StoragedValue(
-        Command.clearDeviceSupport.script,
-        defaultValue: false
-    )
+    // MARK: - Initializers
     
-    var removeOldSimulators = StoragedValue(
-        Command.removeOldSimulators.script,
-        defaultValue: false
-    )
-    
-    var clearSimulatorData = StoragedValue(
-        Command.clearSimulatorData.script,
-        defaultValue: false
-    )
-    
-    var resetXcodePreferences = StoragedValue(
-        Command.resetXcodePreferences.script,
-        defaultValue: false
-    )
-    
-    var displayFreeUpSpaceInMenuBar = StoragedValue(
-        "display-free-up-space-in-menu-bar",
-        defaultValue: true
-    )
-    
-    var launchAtLogin = StoragedValue(
-        "launch-at-login",
-        defaultValue: true
-    )
+    init(_ userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+        
+        removeArchives = .init(
+            Command.removeArchives.script,
+            defaultValue: true,
+            userDefaults: userDefaults
+        )
+        
+        removeCaches = .init(
+            Command.removeCaches.script,
+            defaultValue: true,
+            userDefaults: userDefaults
+        )
+        
+        removeDerivedData = StoragedValue(
+            Command.removeDerivedData.script,
+            defaultValue: true,
+            userDefaults: userDefaults
+        )
+        
+        clearDeviceSupport = StoragedValue(
+            Command.clearDeviceSupport.script,
+            defaultValue: false,
+            userDefaults: userDefaults
+        )
+        
+        removeOldSimulators = StoragedValue(
+            Command.removeOldSimulators.script,
+            defaultValue: false,
+            userDefaults: userDefaults
+        )
+        
+        clearSimulatorData = StoragedValue(
+            Command.clearSimulatorData.script,
+            defaultValue: false,
+            userDefaults: userDefaults
+        )
+        
+        resetXcodePreferences = StoragedValue(
+            Command.resetXcodePreferences.script,
+            defaultValue: false,
+            userDefaults: userDefaults
+        )
+        
+        displayFreeUpSpaceInMenuBar = StoragedValue(
+            "display-free-up-space-in-menu-bar",
+            defaultValue: true,
+            userDefaults: userDefaults
+        )
+        
+        launchAtLogin = StoragedValue(
+            "launch-at-login",
+            defaultValue: true,
+            userDefaults: userDefaults
+        )
+    }
     
 }
 
